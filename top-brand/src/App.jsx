26f28e3fd12f48874d49c799d74e3b4f74b53ca1
paul <ampaulkaranja@gmail.com>
2025-12-06 19:39:36 +0300

    Fix React Router for GitHub Pages

--- top-brand/src/App.jsx
+++ top-brand/src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState }  from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop.jsx';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
@@ -8,21 +8,18 @@ import Contact from './pages/Contact.jsx';
 import Cart from './pages/Cart.jsx';
 
 const App = () => {
-  
   return (
-    
-  <Router>
-        <ScrollToTop />
-        <Routes>
-          {/* <Route path="*" element={<NotFound />} /> */}
-          <Route path="/" element={<Home/>} />
-          <Route path="about" element={<About/>} />
-          <Route path="brands" element={<Brands/>} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="cart" element={<Cart/>} />
-        </Routes>
-  </Router>
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="about" element={<About/>} />
+        <Route path="brands" element={<Brands/>} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="cart" element={<Cart/>} />
+      </Routes>
+    </Router>
   )
 }
 
-export default App
+export default App;
